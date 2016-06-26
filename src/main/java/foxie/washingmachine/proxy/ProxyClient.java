bee799d40845aa7647a99b095e85808b0790be93
Ashley <github@victorianfox.com>
2016-06-26 18:47:42 +0200

    Better way to register json for washing powder

--- src/main/java/foxie/washingmachine/proxy/ProxyClient.java
+++ src/main/java/foxie/washingmachine/proxy/ProxyClient.java
@@ -1,20 +1,18 @@
 package foxie.washingmachine.proxy;
 
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraftforge.client.model.ModelLoader;
 
 public class ProxyClient extends ProxyCommon {
    @Override
    public void preinit() {
       super.preinit();
       ProxyCommon.wm.initModel();
-
+      ModelLoader.setCustomModelResourceLocation(wp, 0, new ModelResourceLocation("washingmachine:washingpowder", "inventory"));
    }
 
    @Override
    public void init() {
       super.init();
-      Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(wp, 0, new ModelResourceLocation("washingmachine:washingpowder", "inventory"));
-
    }
 }
