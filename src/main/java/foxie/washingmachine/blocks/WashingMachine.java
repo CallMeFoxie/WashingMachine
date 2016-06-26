6fd53eebe8c1cade982837a7b3da93e4b1546c14
Ashley <github@victorianfox.com>
2016-06-26 19:28:55 +0200

    Make json registrations identical

--- src/main/java/foxie/washingmachine/blocks/WashingMachine.java
+++ src/main/java/foxie/washingmachine/blocks/WashingMachine.java
@@ -6,11 +6,9 @@ import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.PropertyDirection;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
@@ -19,10 +17,7 @@ import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.common.registry.GameRegistry;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 
 import javax.annotation.Nullable;
 
@@ -37,11 +32,6 @@ public class WashingMachine extends BlockContainer {
       GameRegistry.register(this);
       GameRegistry.register(new ItemBlock(this), getRegistryName());
       setDefaultState(blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH));
-   }
-
-   @SideOnly(Side.CLIENT)
-   public void initModel() {
-      ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(this), 0, new ModelResourceLocation(getRegistryName(), "inventory"));
    }
 
    @Override
--- src/main/java/foxie/washingmachine/proxy/ProxyClient.java
+++ src/main/java/foxie/washingmachine/proxy/ProxyClient.java
@@ -1,13 +1,14 @@
 package foxie.washingmachine.proxy;
 
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.item.Item;
 import net.minecraftforge.client.model.ModelLoader;
 
 public class ProxyClient extends ProxyCommon {
    @Override
    public void preinit() {
       super.preinit();
-      ProxyCommon.wm.initModel();
+      ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(wm), 0, new ModelResourceLocation("washingmachine:washingmachine", "inventory"));
       ModelLoader.setCustomModelResourceLocation(wp, 0, new ModelResourceLocation("washingmachine:washingpowder", "inventory"));
    }
 
